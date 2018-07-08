8557b11bc08aae59426c4cdd724c76e580608039
Peter Pfaffeneder <peter.pfaffeneder@bitmovin.com>
2018-07-08 07:25:22 +0200

    Configured motion settings

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -529,14 +529,14 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 95 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 600, 95 }
 
 /**
  * Default Max Feed Rate (mm/s)
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 500, 500, 15, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 500, 500, 35, 25 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
